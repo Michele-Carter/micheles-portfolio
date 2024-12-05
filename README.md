0ea29fb3a04538d4363f103f3d15290e820da350
Michele-Carter <tumeke4u@gmail.com>
2024-12-05 18:54:11 +1300

    first push

---
+++ README.md
@@ -0,0 +1,26 @@
+Michele Carter
+
+# Portfolio Website
+
+This is my personal portfolio website built with [Next.js](https://nextjs.org/), designed to showcase my projects, skills, and professional experience. This site serves as a portfolio to highlight my work and is a point of contact for potential clients, collaborators, and employers.
+
+## Demo
+
+Check out the live version of my portfolio [here](https://michele-carter-portfolio.vercel.app/) (replace with your actual deployed link).
+
+## Features
+
+- _Responsive Design_: Optimized for all screen sizes, from mobile devices to desktops.
+- _Project Showcase_: Displays a selection of my best projects with live links and descriptions.
+- _About Me_: Details about my background, skills, and professional experience.
+- _Testimonials_: Highlights feedback and recommendations from clients or collaborators.
+- _Dynamic Content_: Easily customizable with JSON data or content management integration.
+
+## Technologies
+
+- [Next.js](https://nextjs.org/)
+- [React](https://reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Tailwind CSS](https://tailwindcss.com/) (or other CSS framework if applicable)
+- [Framer Motion](https://www.framer.com/motion/) for animations
+- [Vercel](https://vercel.com/) for deployment (or specify your deployment platform)
---
+++ data/index.ts
@@ -0,0 +1,251 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Figma clone",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Brainwave",
+    des: "Modern design stack for modern tech.",
+    img: "/Brainwave.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://brainwaveaiconceptbuild.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "iPhone Website Clone",
+    des: "Crisp, clean, modern minimalism",
+    img: "/iPhoneWebsiteClone.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://michele-iphone-clone.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Husk",
+    des: "Design thinking for the digital age.",
+    img: "/husk.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://www.pixelperfectwebdesigns.co.nz/projects/husk",
+  },
+  {
+    id: 4,
+    title: "Burst Digital",
+    des: "A new chapter, a fresh outlook",
+    img: "/burst.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://www.burstdigital.co.nz/social-media",
+  },
+  {
+    id: 5,
+    title: "Autistic Innovations",
+    des: "A custom built, sleek yet soft design",
+    img: "/innovations.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://www.autisticinnovations.co.nz/",
+  },
+  {
+    id: 6,
+    title: "Mac Auto Services",
+    des: "A fresh idea for an outdated design",
+    img: "/macauto.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://www.macautoservices.co.nz/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Michele created an amazing website for my business.  she listened to my ideas and transformed them into a beautiful website.  Michele was great to work with, very attentive and patient and went above and beyond to help me with any questions I had or help I needed.  I would highly recommend!",
+    name: "Karen",
+    image: "/karen.png",
+    title: "Autistic Innovations",
+  },
+  {
+    quote:
+      "I approached Michele to help me design a website that will improve my conversion rates. I was impressed with her level of professionalism and quick turnaround. I was so impressed with my new site which now has seamless conversions from prospective clients! Highly recommend using Michele for your next project.",
+    name: "Roimata",
+    image: "/roi.png",
+    title: "Burst Digital",
+  },
+  {
+    quote:
+      "Michele designed the website for our business MAC Auto Services, she was great to work with and made everything so much easier for me and the design is amazing and bringing in alot of traffic & new customers.",
+    name: "Megan",
+    image: "/megan.png",
+    title: "Mac Auto Services",
+  },
+  {
+    quote:
+      "Michele created an amazing website for my business.  she listened to my ideas and transformed them into a beautiful website.  Michele was great to work with, very attentive and patient and went above and beyond to help me with any questions I had or help I needed.  I would highly recommend!",
+    name: "Karen",
+    image: "/karen.png",
+    title: "Autistic Innovations",
+  },
+  {
+    quote:
+      "I approached Michele to help me design a website that will improve my conversion rates. I was impressed with her level of professionalism and quick turnaround. I was so impressed with my new site which now has seamless conversions from prospective clients! Highly recommend using Michele for your next project.",
+    name: "Roimata",
+    image: "/roi.png",
+    title: "Burst Digital",
+  },
+  {
+    quote:
+      "Michele designed the website for our business MAC Auto Services, she was great to work with and made everything so much easier for me and the design is amazing and bringing in alot of traffic & new customers.",
+    name: "Megan",
+    image: "/megan.png",
+    title: "Mac Auto Services",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "webflow",
+    img: "/webflowIcon.svg",
+  },
+  {
+    id: 2,
+    name: "figma",
+    img: "/figmaIcon.svg",
+  },
+  {
+    id: 3,
+    name: "nextjs",
+    img: "/nextjs.svg",
+  },
+  {
+    id: 4,
+    name: "react",
+    img: "/react.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "framer.",
+    img: "/framer.svg",
+  },
+  {
+    id: 6,
+    name: "js.",
+    img: "/js.svg",
+  },
+  {
+    id: 7,
+    name: "ts.",
+    img: "/typescript.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Freelance Web Designer/Developer",
+    desc: "Over the past two and a half years that I've been learning Web Design and Development, I've produced four live websites for clients using a variety of platforms for personal clients.  Have also created various additional 'concept' sites and projects during my time studying as a Web Developer.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Designer/Content Creator",
+    desc: "Assisted with the design and development of client websites using a proprietary CMS system.  Assisted with the creation of graphical content for use on website such as advertisements and banner.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Diploma in Web & UI/UX Design",
+    desc: "A full time 40 week course studying at Yoobee College of Creative Innovation, where I learnt the art of UX Design and Web Design which included Responsive Design, HTML5, CSS3 and JavaScript.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Diploma in Graphic Design",
+    desc: "A full time 40 week course studying at Yoobee College of Creative Innovation, where I learnt Graphic Design principles such as color theory, the use of white space, typography, layout and how to use the Adobe Creative Suite.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
